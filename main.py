e4df222103f547929218d97d903f56612e4977bf
Vladerie <valerie-veldhuizen@live.nl>
2024-12-31 16:25:17 -0400

    now it is possible to move the player around on the screen

--- main.py
+++ main.py
@@ -12,15 +12,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
+        for obj in updatable:
+            obj.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+        
 
     
 
